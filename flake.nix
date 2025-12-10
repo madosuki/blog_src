52128968cdba69c09b74f45e0940af9dfe907fcc
madosuki <yurikasamaaikatsu@gmail.com>
2025-12-10 16:53:08 +0900

    add flake for pygements

---
+++ flake.nix
@@ -0,0 +1,28 @@
+{
+  description = "for pygments";
+
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs?ref=nixos-unstable";
+    flake-utils.url  = "github:numtide/flake-utils";
+  };
+
+  outputs = { self, nixpkgs, flake-utils }:
+    flake-utils.lib.eachDefaultSystem (system:
+      let
+        pkgs = import nixpkgs {
+          inherit system;
+        };
+      in
+        {
+          devShells.default = with pkgs; mkShell {
+            buildInputs = [
+              python312
+              python312Packages.pygments
+            ];
+            shellHook = ''
+            exec zsh
+            '';
+          };
+        }
+    );
+}
